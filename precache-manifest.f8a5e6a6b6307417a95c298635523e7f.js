5c5f0e6b5b9f77a4f8915aee2f333b4c349cf6c3
brujat <brujat.u@ymedialabs.com>
2019-06-17 20:27:55 +0530

    changed stuff

---
+++ precache-manifest.f8a5e6a6b6307417a95c298635523e7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf8af5899071c7e0d90036b06053df7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0272c0fe5ea7f1f76eba",
+    "url": "/static/css/main.c7e87293.chunk.css"
+  },
+  {
+    "revision": "84aa2778b7cd9bfbbb9f",
+    "url": "/static/js/2.398ff030.chunk.js"
+  },
+  {
+    "revision": "0272c0fe5ea7f1f76eba",
+    "url": "/static/js/main.8bb11b26.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
